2625ba580e6d1b4bcc12526878ff470af28438f5
Max Inman <max.inman101@gmail.com>
2021-10-24 22:48:35 -0400

    10/24/21

--- myfile.py
+++ myfile.py
@@ -1,1 +1,31 @@
-tl_url = ""
+from typing import Text
+import requests
+
+
+tl_url = "https://jujutsu-kaisen.online"
+
+
+def get_source(_tl_url):
+    r = requests.get(_tl_url)
+    r_source = r.text
+    return r_source.split('<li id="su-post')
+
+urls = []
+for row in get_source(tl_url):
+        urls.append(row)
+        
+
+for i in range(1, len(urls)):
+    urls[i] = urls[i].split('href="')[1]
+    urls[i] = urls[i].split('">')[0]
+    if "Chapter 1" in urls[i]:
+        urls[i] = urls[i].split("</li>")[0]
+        
+
+        
+    print(urls[i])
+        
+
+ 
+
+
