25a8cf8d6e34775acf698f17b8fc83611ef2f7cf
Max <max.inman101@gmail.com>
2021-10-26 22:17:29 -0400

    path to chapter links complete

--- myfile.py
+++ myfile.py
@@ -1,63 +1,62 @@
 from typing import Text
 import requests
+from requests.api import get
 
 
 tl_url = "https://jujutsu-kaisen.online"
 
-
+#get source text and split by link beginning
 def get_source(_tl_url):
     r = requests.get(_tl_url)
     r_source = r.text
-    return r_source.split('<a href=')
+    return r_source.split('"><a href="h') #remove first h in https://... for sorting purposes
+    
 
-urls = []
+
+#put items in array
+links = []
 for row in get_source(tl_url):
-        urls.append(row)
+        links.append("h" + row) #adds back that h to the url
         
-
-def trim(_urls, _start, _end, chapter=False):
+        
+    
+def trim(_urls, chapter=False):
+    _list = _urls
+    if chapter:
+        for i in range(1, len(_list)):
+            jpgs = []
+            for row in get_source(_list[i]):
+                jpgs.append("h" + row)
+            #download jpgs
+            
+        pass
+    else:
+        for i in range(1, len(_list)):
+            _list[i] = _list[i].split('/">')[0] # everything before ">
+            
+            if "chapter-1" in _urls[i]:
+                _list[i] = _list[i].split("</li>")[0]
+            
+            
+            for j in range(0, i):
+                if _list[i] == _list[j]:
+                    _list[i] = _list[i] + "$"
+    x = 1                       
     for i in range(1, len(_urls)):
-        if chapter:
-            
-            
-            
-        _urls[i] = _urls[i].split(_start)[1]
-        _urls[i] = _urls[i].split(_end)[0]
-        if(i == len(_urls)):
-            
-                
-                
-  
-  
-  
-  
-            
-for i in range(1, len(urls)):
-    urls[i] = urls[i].split('href="')[1] #everything after href
-    urls[i] = urls[i].split('">')[0] # everything before ">
-    if "Chapter 1" in urls[i]:
-        urls[i] = urls[i].split("</li>")[0]
-    for j in range(0, i):
-        if urls[i] in urls[j]:
-            urls[i] = urls[i] + "$"
-x = 1                       
-for i in range(1, len(urls)):
-    if "$" in urls[i]:
-        x = x + 1
+        if "$" in _list[i]:
+            x = x + 1
 
-for k in range(len(urls) - 1, (len(urls) - x), -1):
-    urls.remove(urls[k])
-
-
-for i in range(1, len(urls)):
-     source = get_source(urls[i]) 
+    for k in range(len(_list) - 1, (len(_list) - x), -1):
+        _urls.remove(_list[k])
         
 
-    
-    
-
-        
-
+    return _list
+                    
  
+                    
+    
+        
+    
+                    
 
-
+print(trim(trim(links), chapter=False)[])
